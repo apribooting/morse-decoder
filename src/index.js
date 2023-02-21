b38a96da87c5eaec4461e4af7900adcca3b0a73d
apribooting <roflanPushka@gmail.com>
2023-02-21 22:53:06 +0300

    feat: done task

--- src/index.js
+++ src/index.js
@@ -38,9 +38,52 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+  let keys = Object.keys(MORSE_TABLE);
+  let values = Object.values(MORSE_TABLE);
+  let prob = "**********";
+  let arr = [];
+  for (let i = 0; i < expr.length / 10; i++) {
+    arr.push(expr.slice(i * 10, i * 10 + 10));
+  }
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === prob) {
+      arr[i] = " ";
+    } else {
+      arr[i] = getStr(arr[i]);
+      arr[i] = arr[i].match(/.{1,2}/g);
+      arr[i] = getSymbol(arr[i]);
+      for (let j = 0; j < keys.length; j++) {
+        if (arr[i] === keys[j]) {
+          arr[i] = values[j];
+        }
+      }
+    }
+  }
+  function getStr(str) {
+    let word = "";
+    for (let i = 0; i < str.length; i++) {
+      if (str[i] === "1") {
+        return (word += str.slice(i));
+      }
+    }
+    for (let i = 0; i < word.length; i++) {
+      word = word.slice(i * 2, i * 2 + 2);
+    }
+    return word;
+  }
+  function getSymbol(str) {
+    let sym = "";
+    for (let i = 0; i < str.length; i++) {
+      if (str[i] === "11") {
+        sym += "-";
+      } else {
+        sym += ".";
+      }
+    }
+    return sym;
+  }
+  return arr.join("");
 }
-
 module.exports = {
-    decode
-}
+  decode,
+};
